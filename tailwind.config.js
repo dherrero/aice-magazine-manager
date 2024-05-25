31f79799a362f6f9d3a9580c84cf5f7cb3dd8363
Daniel Herrero <dherrero82@gmail.com>
2024-05-25 20:52:57 +0200

    chore: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./apps/app-front/src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
