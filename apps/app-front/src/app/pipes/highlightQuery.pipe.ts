1ccabcd2fad7dbfbe1aef806bc905413bb11f9f9
Daniel Herrero <dherreto@mercadona.es>
2024-05-28 08:20:31 +0200

    feat: card sizing and responsive layout

--- apps/app-front/src/app/pipes/highlightQuery.pipe.ts
+++ apps/app-front/src/app/pipes/highlightQuery.pipe.ts
@@ -6,6 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HighlightQueryPipe implements PipeTransform {
   transform(content: string, query: string): string {
+    const maxWords = 100;
     const words = content.split(' ').flat();
     const lowerWords = content.toLowerCase().split(' ').flat();
     const queryIndex = lowerWords.indexOf(query.toLowerCase());
@@ -21,15 +22,15 @@ export class HighlightQueryPipe implements PipeTransform {
       // Si la consulta no se encuentra en el contenido, simplemente devuelve el contenido truncado
       return (
         words
-          .slice(0, 50)
+          .slice(0, maxWords)
           .join(' ')
           .replace(regex, `<span class='highlight'>${match[0]}</span>`) +
-        (words.length > 50 ? '...' : '')
+        (words.length > maxWords ? '...' : '')
       );
     } else {
       // Si la consulta se encuentra en el contenido, devuelve una porción del contenido centrada en la consulta
-      const start = Math.max(0, queryIndex - 25);
-      const end = Math.min(words.length, queryIndex + 25);
+      const start = Math.max(0, queryIndex - maxWords / 2);
+      const end = Math.min(words.length, queryIndex + maxWords / 2);
       return (
         (start > 0 ? '...' : '') +
         words
