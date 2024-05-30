11f9086bf68e4aa62517288909045255cb213197
Daniel Herrero <dherreto@mercadona.es>
2024-05-30 17:22:06 +0200

    feat: back office routing

--- apps/app-front/src/app/app.routes.ts
+++ apps/app-front/src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const appRoutes: Route[] = [
     path: '',
     loadComponent: () => import('./pages/home/home.component'),
   },
+  {
+    path: 'back-office',
+    loadComponent: () => import('./pages/back-office/back-office.component'),
+  },
 ];
---
+++ apps/app-front/src/app/pages/back-office/back-office.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back-office',
+  standalone: true,
+  templateUrl: './back-office.component.html',
+  styleUrls: ['./back-office.component.scss'],
+})
+export default class BackOfficeComponent {}
